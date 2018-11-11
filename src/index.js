b0cea58adb6da95f705f52ef5ba6958ca53e317b
jsphbtst <joseph.bautista.upd.edu.ph>
2018-11-11 14:41:22 +0800

    Removed setState and turned App into a functional component

--- src/index.js
+++ src/index.js
@@ -2,61 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../style/style.css';
 
-class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			color: ''
-		};
-		this.changeColor = this.changeColor.bind(this);
-	}
-
-	changeColor(color) {
-		this.setState({ color: color }, () => {
-			document.body.style.backgroundColor = this.state.color;
-		});
-	}
-
-	render() {
-		return(
-			<div className="App">
-				<div className="row">
-					<div className="col-md-1"></div>
-					<div className="col-md-2">
-						<button className="btn" 
-							onClick={() => this.changeColor('#ed6a5a')}>
-							ed6a5a
-						</button>
-					</div>
-					<div className="col-md-2">
-						<button className="btn" 
-							onClick={() => this.changeColor('#fbfffe')}>
-							fbfffe
-						</button>
-					</div>
-					<div className="col-md-2">
-						<button className="btn" 
-							onClick={() => this.changeColor('#f4f1bb')}>
-							f4f1bb
-						</button>
-					</div>
-					<div className="col-md-2">
-						<button className="btn" 
-							onClick={() => this.changeColor('#f6a90a')}>
-							f6a90a
-						</button>
-					</div>
-					<div className="col-md-2">
-						<button className="btn" 
-							onClick={() => this.changeColor('#9bc1bc')}>
-							9bc1bc
-						</button>
-					</div>
-					<div className="col-md-1"></div>
+const App = () => {
+	return(
+		<div className="App">
+			<div className="row">
+				<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<button className="btn" 
+						onClick={() => {
+							document.body.style.backgroundColor = '#ed6a5a';
+						}}>
+						ed6a5a
+					</button>
+				</div>
+				<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<button className="btn" 
+						onClick={() => {
+							document.body.style.backgroundColor = '#fbfffe';
+						}}>
+						fbfffe
+					</button>
+				</div>
+				<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<button className="btn" 
+						onClick={() => {
+							document.body.style.backgroundColor = '#f6a90a';
+						}}>
+						f6a90a
+					</button>
+				</div>
+				<div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+					<button className="btn" 
+						onClick={() => {
+							document.body.style.backgroundColor = '#9bc1bc';
+						}}>
+						9bc1bc
+					</button>
 				</div>
 			</div>
-		);
-	}
+		</div>
+	);
 }
 
 ReactDOM.render(
